a35ae7084def413b175ea1e34e615c90e698c47e
lingrd <121781992+tuanhqv123@users.noreply.github.com>
2025-02-01 09:57:40 +0700

    Update index.ts

--- index.ts
+++ index.ts
@@ -20,7 +20,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      // ...other origins...
+      "https://portfolio-frontend-virid-two.vercel.app",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
